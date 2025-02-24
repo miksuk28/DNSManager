5433284ab3acda67902cd743c7e204c6318dc8ad
miksuk28 <miksuk28@outlook.com>
2025-02-24 21:27:41 +0100

    Adding basic methods

--- manager.py
+++ manager.py
@@ -1,15 +1,28 @@
 import requests
+import config
+import sql_statements as sql
+from db_manager import DatabaseConnection
 
 class TechnitiumException(Exception):
-    def __init__(self, status, error):            
+    def __init__(self, status, error, stacktrace=None):
+        # Do logging stuff here
+        print("STACKTRACE WOULD BE HERE")
         # Call the base class constructor with the parameters it needs
         super().__init__(error)
 
 
+class ManagerException(Exception):
+    def __init__(self, status, error):
+        # Do logging stuff here
+        print("Inside ManagerException wooo")
+        super().__init__(error)
 
-class Manager:
+
+class Manager(DatabaseConnection):
     def __init__(self, token, address, port="5380", schema="http"):
+        super().__init__()
         self.URL = f"{schema}://{address}:{port}/api"
+        self.comment = config.MANAGER_DEFAULT_COMMENT
         self.token = token
 
     
@@ -22,7 +35,7 @@ class Manager:
         data = r.json()
 
         if data["status"] != "ok":
-            raise TechnitiumException(data["status"], data["errorMessage"])
+            raise TechnitiumException(data["status"], data["errorMessage"], data.get("stackTrace"))
 
         return data["response"]
 
@@ -104,21 +117,75 @@ class Manager:
         )
 
 
-    def add_dns_a_record(self, domain, address, zone="", overwrite=False, comments=""):     
-        r = self._request(
-            "/zones/records/add",
-            params={
-                "domain": domain,
-                "zone": zone,
-                "type": "A",
-                "ipAddress": address,
-                "overwrite": str(overwrite),
-                "comments": comments
-            }
-        )
+    def _get_domain_by_id(self, domain_id):
+        with self.cur() as cur:
+            cur.execute(sql.GET_DOMAIN_BY_ID, (domain_id,))
+            domain = cur.fetchone()
+
+            if not domain:
+                raise ManagerException("error", f"Domain with id {domain_id} does not exist")
+
+            return domain[0]
 
 
-    def add_dns_cname_record(self, domain, target, zone=None, overwrite=False, comments=""):
+    def _get_domain_id(self, domain):
+        with self.cur() as cur:
+            cur.execute(sql.GET_DOMAIN_ID, (domain,))
+            domain_id = cur.fetchone()
+
+            if not domain:
+                raise ManagerException("error", f"Domain {domain} does not exist")
+
+            return domain_id[0]
+
+
+    def _get_dhcp_scope_id(self, dhcp_scope):
+        with self.cur() as cur:
+            cur.execute(sql.GET_DHCP_SCOPE_ID, (dhcp_scope,))
+            dhcp_scope_id = cur.fetchone()
+
+            if not dhcp_scope_id:
+                raise ManagerException("error", f"DHCP Scope {dhcp_scope} does not exist")
+
+            return dhcp_scope_id[0]
+
+
+    def add_host(self, hostname, domain, address, managed_dhcp, mac_address=None, dhcp_scope=None, overwrite=False, comments=""):     
+        domain_id = self._get_domain_id(domain)
+        dhcp_scope_id = self._get_dhcp_scope_id(dhcp_scope)
+
+        with self.cur() as cur:
+            cur.execute(sql.ADD_HOST, {
+                "hostname":     hostname,
+                "domainId":     domain_id,
+                "ipAddress":    address,
+                "managedDhcp":  managed_dhcp,
+                "dhcpScopeId":  dhcp_scope_id,
+                "macAddress":   mac_address
+            })
+            r = self._request(
+                "/zones/records/add",
+                params={
+                    "domain": f"{hostname}.{domain}",
+                    #"zone": zone,
+                    "type": "A",
+                    "ipAddress": address,
+                    "overwrite": True,
+                    "comments": self.comment
+                }
+            )
+
+            if managed_dhcp:
+                self.add_static_dhcp_lease(
+                    scope=dhcp_scope,
+                    hardware_address=mac_address,
+                    ip_address=address,
+                    hostname=f"{hostname}.{domain}",
+                    comments=self.comment
+                )
+
+
+    def add_dns_cname_record(self, domain, target, zone=None):
         r = self._request(
             "/zones/records/add",
              params={
@@ -126,8 +193,8 @@ class Manager:
                 "zone": zone,
                 "type": "CNAME",
                 "cname": target,
-                "overwrite": str(overwrite),
-                "comments": comments
+                "overwrite": True,
+                "comments": self.comment
             }
         )
 
@@ -147,3 +214,11 @@ class Manager:
             "/zones/records/delete",
             params=params
         )
+
+
+
+# for testing
+api = Manager(
+    token=config.TECHNITIUM_TOKEN,
+    address=config.TECHNITIUM_ADDRESS
+)
--- sql_statements.py
+++ sql_statements.py
@@ -1,1 +1,5 @@
-GET_ALL_HOSTS='SELECT * FROM hosts'
+GET_DOMAIN_BY_ID="SELECT domainName FROM domains WHERE domainId=?"
+GET_DOMAIN_ID="SELECT domainId FROM domains WHERE domainName=?"
+ADD_HOST='''INSERT INTO hosts (hostname, domainId, ipAddress, managedDhcp, dhcpScopeId, macAddress)
+            VALUES (:hostname, :domainId, :ipAddress, :managedDhcp, :dhcpScopeId, :macAddress)'''
+GET_DHCP_SCOPE_ID="SELECT dhcpScopeId FROM dhcpScopes WHERE dhcpScopeName=?"
