de1f1c8adabb7d77693c4b49741ec6d54a8b7761
miksuk28 <miksuk28@outlook.com>
2025-03-07 21:01:19 +0100

    Removed "Managed" field from HostsTable

--- frontend/src/components/HostsTable.vue
+++ frontend/src/components/HostsTable.vue
@@ -6,7 +6,7 @@
           <th scope="col">Hostname</th>
           <th scope="col">IP Address</th>
           <th scope="col">Managed</th>
-          <th scope="col">MAC Address</th>
+          <!--<th scope="col">MAC Address</th>-->
           <th scope="col">DHCP Scope</th>
           <!--<th scope="col"></th>-->
         </tr>
@@ -15,7 +15,7 @@
         <tr v-for="host in hosts" :key="host.hostId">
           <td><a @click="this.$router.push(`/hosts/${host.hostId}`)">{{ host.hostname }}</a></td>
           <td>{{ host.ipAddress }}</td>
-          <td>{{ host.managedDhcp === 1 ? "Yes" : "No" }}</td>
+          <!--<td>{{ host.managedDhcp === 1 ? "Yes" : "No" }}</td>-->
           <td>{{ host.macAddress || '—'}}</td>
           <td>{{ host.dhcpScopeName }}</td>
           <!--<td><small><button class="outline primary">+</button></small></td>-->
