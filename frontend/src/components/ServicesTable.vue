766eb37d6146591d1b13c837e7d677d0473e249e
miksuk28 <miksuk28@outlook.com>
2025-03-09 16:31:47 +0100

    Service names are not links

--- frontend/src/components/ServicesTable.vue
+++ frontend/src/components/ServicesTable.vue
@@ -9,7 +9,7 @@
     </thead>
     <tbody>
       <tr v-for="service in services" :key="service.serviceId">
-        <td>{{ service.domainName }}</td>
+        <td><a :href="`http://${service.domainName}`" target="_blank">{{ service.domainName }}</a></td>
         <td>{{ service.description || '—' }}</td>
         <td><a @click="this.$router.push(`/hosts/${service.targetHostId}`)">{{ service.target }}</a></td>
       </tr>
