97f8b2d422ff2b48bdb070a6e35a0f769ade13fa
miksuk28 <miksuk28@outlook.com>
2025-03-21 22:58:17 +0100

    modified layout

--- ManagerFrontend/src/components/Layout.vue
+++ ManagerFrontend/src/components/Layout.vue
@@ -4,7 +4,7 @@
   <v-navigation-drawer>
     <v-list nav>
       <v-list-item @click="this.$router.push('/hosts')" title="Host management" link></v-list-item>
-      <v-list-item title="Servers" link></v-list-item>
+      <v-list-item @click="this.$router.push('/servers')" title="Servers" link></v-list-item>
       <v-list-item @click="this.$router.push('/services')" title="Services" link></v-list-item>
     </v-list>
   </v-navigation-drawer>
