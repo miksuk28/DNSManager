a4d9d96aabe1c30dae7e0d59053c51523f8a25e9
miksuk28 <miksuk28@outlook.com>
2025-03-21 18:41:21 +0100

    added service page

--- ManagerFrontend/src/pages/services.vue
+++ ManagerFrontend/src/pages/services.vue
@@ -1,15 +1,35 @@
 <template>
-  <v-main>
+  <v-main class="d-flex flex-row">
+    <v-tabs v-model="this.tab" >
+      <v-tab prepend-icon="mdi-web" text="List services" value="servicesList"></v-tab>
+      <v-tab prepend-icon="mdi-plus" text="Add Service" value="addService"></v-tab>
+    </v-tabs>
+    
+    
     <v-container>
-      <v-data-table :items="this.services" :headers="this.tableHeaders" ></v-data-table>
+      <v-tabs-window v-model="this.tab">
+
+        <v-tabs-window-item value="servicesList">
+          <v-data-table :items="this.services" :headers="this.tableHeaders" ></v-data-table>
+        </v-tabs-window-item>
+
+        <v-tabs-window-item value="addService">
+          <AddServiceForm />
+        </v-tabs-window-item>
+
+      </v-tabs-window>
     </v-container>
   </v-main>
 </template>
 
 <script>
+  import AddServiceForm from '@/components/AddServiceForm.vue'
+
   export default {
+    components: { AddServiceForm },
     data() {
       return {
+        tab: null,
         services: [],
         tableHeaders: [
           { title: "FQDN", value: "domainName"},
