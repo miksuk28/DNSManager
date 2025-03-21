4e8b401ca6c56844b02e7550a56955b68b92e1b2
miksuk28 <miksuk28@outlook.com>
2025-03-21 22:59:03 +0100

    added servers page

---
+++ ManagerFrontend/src/pages/servers.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-main class="d-flex flex-row">
+    <v-tabs v-model="this.tab" direction="vertical">
+      <v-tab
+        prepend-icon="mdi-server"
+        text="Servers"
+        value="serverList">
+      </v-tab>
+      <v-tab
+        prepend-icon="mdi-shield-refresh"
+        text="Patching Groups"
+        values="patchingGroups">
+      </v-tab>
+      <v-tab
+        prepend-icon="mdi-history"
+        text="Logs"
+        values="domains">
+      </v-tab>
+    </v-tabs>
+  </v-main>
+</template>
+
+<script>
+
+</script>
