792bbaabff44be51031672fd014b93f77854ef42
miksuk28 <miksuk28@outlook.com>
2025-03-21 18:40:57 +0100

    added error dialog

---
+++ ManagerFrontend/src/components/ErrorDialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-dialog width="auto">
+    <v-card prepent-icon="mdi-alert-circle" title="An Error Occured">
+      <v-card-text>
+        <p>Manager returned the following exception with the code {{ error.status }}</p>
+      </v-card-text>
+
+      <v-card-text>
+        <code>{{ error.exception }}: {{ error.response }}</code>
+      </v-card-text>
+
+      <template v-slot:actions>
+        <v-btn
+          class="ms-auto"
+          text="OK"
+          @click="$emit('close')"
+        ></v-btn>
+      </template>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+  export default {
+    props: ["error"]
+  }
+</script>
